1ba4d3b1b829800a26e25e326b4e2770dbee7b60
Brandom <brandom.quispe2020@gmail.com>
2026-05-22 15:54:00 -0300

    feat: implementa autenticação JWT

---
+++ app/auth.py
@@ -0,0 +1,24 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_senha(senha):
+    return pwd_context.hash(senha)
+
+def verificar_senha(senha, hash):
+    return pwd_context.verify(senha, hash)
+
+def criar_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=60)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
